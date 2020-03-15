c54eafdb3666bcc29edc2045bf5a4bf784c92142
Drachbar <drachbar@gmail.com>
2020-03-15 01:12:32 +0100

    fetch

---
+++ scripts.js
@@ -0,0 +1,30 @@
+console.log("About to fetch rainbow");
+
+catchText()
+  .then(textfile => {
+    document.getElementById("textfile").innerText = textfile;
+  })
+  .catch(err => {
+    console.log("Error!");
+    console.error(err);
+  });
+
+catchRainbow()
+  .then(response => {
+    console.log("YAY");
+  })
+  .catch(err => {
+    console.log("Error!");
+    console.error(err);
+  });
+
+async function catchRainbow() {
+  const response = await fetch("rainbow.jpeg");
+  const blob = await response.blob();
+  document.getElementById("rainbow").src = URL.createObjectURL(blob);
+}
+
+async function catchText() {
+  const responseText = await fetch("testTextFile.txt");
+  return await responseText.text();
+}
